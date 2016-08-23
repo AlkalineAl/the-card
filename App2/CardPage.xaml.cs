c18fe0dc790d2fff00a4315e008ac3d4551ec539
alkalineal <alkalineal54@gmail.com>
2016-08-23 23:23:18 +0300

    Добавьте файлы проекта.

---
+++ App2/CardPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Шаблон элемента пустой страницы задокументирован по адресу http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App2
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class CardPage : Page
+    {
+        public CardPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Damabutton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog msgbox = new MessageDialog("Дама", "Внимание");
+            await msgbox.ShowAsync();
+        }
+
+        private async void Valetbutton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog msgbox = new MessageDialog("Валет", "Внимание");
+            await msgbox.ShowAsync();
+        }
+
+        private async void Kingbutton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog msgbox = new MessageDialog("Король", "Внимание");
+            await msgbox.ShowAsync();
+        }
+
+        private async void Abutton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog msgbox = new MessageDialog("Туз", "Внимание");
+            await msgbox.ShowAsync();
+        }
+
+        private async void Bbutton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog msgbox = new MessageDialog("Бубны", "Внимание");
+            await msgbox.ShowAsync();
+        }
+
+        private async void Pbutton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog msgbox = new MessageDialog("Пики", "Внимание");
+            await msgbox.ShowAsync();
+        }
+
+        private async void Cbutton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog msgbox = new MessageDialog("Чирвы", "Внимание");
+            await msgbox.ShowAsync();
+        }
+
+        private async void Tbutton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog msgbox = new MessageDialog("Трефы", "Внимание");
+            await msgbox.ShowAsync();
+        }
+    }
+}
---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App2
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ContentFrame.Navigate(typeof(CardPage));
+
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+             AppViewBackButtonVisibility.Visible;
+
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested += TestView_BackRequested;
+            radioButton1.Background = new SolidColorBrush(Colors.Red);
+            radioButton2.Background = new SolidColorBrush(Colors.Transparent);
+        }
+
+        private void TestView_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (ContentFrame.CanGoBack)
+            {
+                e.Handled = true;
+                ContentFrame.GoBack();
+                if (ContentFrame.Content.ToString() == "App2.MainDiary")
+                {
+                    radioButton1.IsChecked = true;
+                    radioButton1.Background = new SolidColorBrush(Colors.Red);
+                    radioButton2.Background = new SolidColorBrush(Colors.Transparent);
+                }
+                if (ContentFrame.Content.ToString() == "App2.MapPage")
+                {
+                    radioButton2.IsChecked = true;
+                    radioButton2.Background = new SolidColorBrush(Colors.Red);
+                    radioButton1.Background = new SolidColorBrush(Colors.Transparent);
+                }
+             }
+
+          }
+
+        private void radioButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MySplit.IsPaneOpen)
+            {
+                MySplit.IsPaneOpen = false;
+            }
+            else
+            {
+                MySplit.IsPaneOpen = true;
+            }
+            radioButton.IsChecked = false;
+        }
+
+        private void radioButton1_Click(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(CardPage));
+
+
+            radioButton1.IsChecked = true;
+            radioButton1.Background = new SolidColorBrush(Colors.Red);
+            radioButton2.Background = new SolidColorBrush(Colors.Transparent);
+
+        }
+
+        private void radioButton2_Click(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(SettingsPage));
+            radioButton2.IsChecked = true;
+            radioButton2.Background = new SolidColorBrush(Colors.Red);
+            radioButton1.Background = new SolidColorBrush(Colors.Transparent);
+
+        }
+    }
+}
